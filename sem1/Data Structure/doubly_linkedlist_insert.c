3e35f60e3ae1cae53a2b1c7e3f2cc5bf02e7c024
Rahbar Zahid S <96964081+srahbarzahid@users.noreply.github.com>
2024-10-30 15:08:55 +0530

    Update doubly_linkedlist_insert.c

--- sem1/Data Structure/doubly_linkedlist_insert.c
+++ sem1/Data Structure/doubly_linkedlist_insert.c
@@ -62,6 +62,7 @@ struct node
      {
      	int pos,data,i=1;
      	struct node*temp;
+     	newnode = (struct node*)malloc(sizeof(struct node));
      	printf("enter the position");
      	scanf("%d",&pos);
      	
@@ -78,13 +79,49 @@ struct node
 	  	  	i++;
 	  	  }
 	  	printf("enter the data");
-     	        scanf("%d",&data);
+     	        scanf("%d",&newnode->data);
      	        
 	  	newnode->pre=temp;
 	  	newnode->next=temp->next;
 	  	temp->next=newnode;
 	  	newnode->next->pre=newnode;  
 	  }   	     			
+     }
+     
+       void search()
+     {
+     	int data,flag=0,count=1;
+     	struct node*temp;
+     	printf("enter the data to be search");
+     	scanf("%d",&data);
+     	
+     	if(head == NULL)
+     		{
+     			printf("the list is empty");
+     			return;
+     		}
+     	else
+     	  {
+     		temp=head;
+     		while(temp!=0)
+     		    {
+     		    	if(data==temp->data)
+     		    	    {
+     		    	    flag=1;
+     		    	    break;
+     		    	    }
+     		    	temp=temp->next;
+     		    	count++;
+     		    
+     		    }
+     	  }
+     	  if(flag==1){
+     	      printf("the value is found at %d",count);}
+         else{  
+              printf("the value cannot be find");
+     	     }
+     	  	
+     		
      }
          
    void display()
@@ -104,6 +141,7 @@ struct node
    	 }	
    }
    
+   
    int main()
    {
    int opt;
@@ -113,7 +151,8 @@ struct node
    	 	printf("2.insert at end\n");
    	 	printf("3.insert at specified position\n");
    	 	printf("4.display\n"); 
-   	 	printf("5.exit\n");
+   	 	printf("5.search\n");
+   	 	printf("6.exit\n");
    	 	printf("choose an option ");
    	 	scanf("%d",&opt);
    	 	
@@ -126,8 +165,10 @@ struct node
 		     case 3: middle(); 
 		             break;
 		     case 4: display();
-		             break;			             
-		     case 5: exit(0);        
+		             break;	
+		     case 5: search();
+		     	      break;  	        		             
+		     case 6: exit(0);        
 		     default: printf("invalid option\n");                 		       	   	 	            
    	 	   }
    	 }while(opt!=0);
