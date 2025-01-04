4ee78d05e6a5c4d76169c25b630625e5290f3cfc
Rahbar Zahid S <96964081+srahbarzahid@users.noreply.github.com>
2025-01-04 15:44:21 +0530

    Update bfs.c

--- sem1/Data Structure/bfs.c
+++ sem1/Data Structure/bfs.c
@@ -1,45 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 100
+int graph[MAX][MAX], visited[MAX];
+int v;
 
-#define MAX 100  // Maximum number of vertices
-
-void bfs(int graph[MAX][MAX], int visited[MAX], int start, int n) {
-    int queue[MAX], front = 0, rear = 0;
-    visited[start] = 1;
-    queue[rear++] = start;
-
-    printf("BFS Traversal: ");
-    while (front < rear) {
-        int current = queue[front++];
-        printf("%d ", current);
-
-        for (int i = 0; i < n; i++) {
-            if (graph[current][i] == 1 && !visited[i]) {
-                visited[i] = 1;
-                queue[rear++] = i;
-            }
-        }
-    }
-    printf("\n");
-}
-
-int main() {
-    int graph[MAX][MAX], visited[MAX] = {0}, n, start;
-
-    printf("Enter the number of vertices: ");
-    scanf("%d", &n);
-
-    printf("Enter the adjacency matrix:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            scanf("%d", &graph[i][j]);
-        }
-    }
-
-    printf("Enter the starting vertex: ");
-    scanf("%d", &start);
-
-    bfs(graph, visited, start, n);
-
-    return 0;
-}
+void bfs(int vertex){
+	int queue[MAX],rear=0,front=0,current;
+	visited[vertex]=1;
+	queue[rear++]=vertex;
+	
+	printf("BFS Traversal: ");
+	while(front<rear){
+		current = queue[front++];
+		printf("%d ",current);
+		
+		for(int i=0;i<v;i++){
+			if(graph[current][i]==1 && !visited[i]){
+				visited[i]=1;
+				queue[rear++]=i;
+				}
+			}
+		}
+	}
+	int main(){
+		int start;
+		printf("enter number of vertices");
+		scanf("%d",&v);
+		
+		printf("adjancy matrix");
+		for(int i=0;i<v;i++){
+			for(int j=0;j<v;j++){
+				scanf("%d",&graph[i][j]);
+				}
+			}
+			for(int i=0;i<v;i++){
+				visited[i]=0;
+				}
+				
+				printf("starting vertex please: ");
+				scanf("%d",&start);
+				bfs(start);
+		}
