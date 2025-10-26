5cb084a8bdfbfa45b0c448ddfa4cb241fd6b19fb
Rahbar Zahid S <96964081+srahbarzahid@users.noreply.github.com>
2025-10-26 10:29:59 +0530

    Delete sem3/co1_multiply/bhagya directory

--- sem3/co1_multiply/bhagya/series.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-my_list = [10, 20, 30, 40, 50]
-my_series = pd.Series(my_list)
-
-print("Original list:", my_list)
-print("Converted Pandas Series:")
-print(my_series)
--- sem3/co1_multiply/bhagya/swap.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-nums=np.arange(16,dtype='int').reshape(-1,4)
-print("original array:")
-print(nums)
-print("\nNew array after swapping first and last rows of the said array:")
-nums=nums[[-1,1,2,0]]
-print(nums)
